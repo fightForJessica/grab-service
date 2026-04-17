3a785683e4bd5fca524e26b3d379d91156b8828b
xjw <1010816238@qq.com>
2026-04-17 16:53:38 +0800

    fix:详情页切换深色模式时主题色未更新

--- app/src/main/java/com/jessi/grabservice/DetailActivity.kt
+++ app/src/main/java/com/jessi/grabservice/DetailActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import com.jessi.grabservice.model.AppInfo
 import com.jessi.grabservice.model.HttpReq
 import com.jessi.grabservice.model.HttpRsp
 import com.jessi.grabservice.ui.detail.Detail
+import com.jessi.grabservice.ui.theme.ThemeManager
 import com.jessi.grabservice.utils.Logger
 
 class DetailActivity : ComponentActivity() {
@@ -32,6 +35,12 @@ class DetailActivity : ComponentActivity() {
         val response = intent.getParcelableExtra(KEY_RESPONSE) as? HttpRsp
         val initPageIndex = intent.getIntExtra(KEY_INIT_PAGE_INDEX, 0)
         setContent {
+
+            // 主题色适配
+            val isSystemInDarkTheme = isSystemInDarkTheme()
+            ThemeManager.updateTheme(isSystemInDarkTheme)
+            Logger.i("DetailActivity: theme isDark: $isSystemInDarkTheme")
+
             Scaffold(Modifier.fillMaxSize()) { innerPadding ->
                 // intent 传递参数
                 Detail(
